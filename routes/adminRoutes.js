bc277c15e24bafb40db6e6c04da43a67aae9eab1
unknown <azril.dap24@gmail.com>
2025-06-22 21:50:11 +0700

    fix: fixing admin middleware in adminroute

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,13 +9,13 @@ const { authMiddleware, authorizeRoles, authorize, authorizeByRoute } = require(
 // router.get('/', authMiddleware, authorizeRoles('admin'), authorize('manage_users'), adminController.getAllTrash);
 router.get('/trashes/all', authMiddleware, authorizeByRoute, adminController.getAllTrash);
 // router.put('/verif/:id', authMiddleware, adminController.verifikasiSampah);
-router.patch('/verif/:id', authMiddleware, adminController.verifikasiSampah);
-router.get('/transaction/all', authMiddleware, adminController.getAllTransactions);
-router.get('/user/all', authMiddleware, adminController.getAllUsers);
-router.get('/role/all', authMiddleware, adminController.getAllRoles);
-router.get('/trashtype/all', authMiddleware, adminController.getAllTrashTypes);
-router.post('/trashtype/create', authMiddleware, adminController.createTrashType);
-router.patch('/trashtype/edit/:id', authMiddleware, adminController.updateTrashType);
+router.patch('/verif/:id', authMiddleware, authorizeRoles('admin'), adminController.verifikasiSampah);
+router.get('/transaction/all', authMiddleware, authorizeRoles('admin'), adminController.getAllTransactions);
+router.get('/user/all', authMiddleware, authorizeRoles('admin'), adminController.getAllUsers);
+router.get('/role/all', authMiddleware, authorizeRoles('admin'), adminController.getAllRoles);
+router.get('/trashtype/all', authMiddleware, authorizeRoles('admin'), adminController.getAllTrashTypes);
+router.post('/trashtype/create', authMiddleware, authorizeRoles('admin'), adminController.createTrashType);
+router.patch('/trashtype/edit/:id', authMiddleware, authorizeRoles('admin'), adminController.updateTrashType);
 
 router.get('/dashboard', authMiddleware, adminController.getDashboardStats);
 
